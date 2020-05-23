87093c88aef6d8703845768e171972e1c039204f
Emre <emre@bigoen.com>
2020-05-23 23:31:37 +0300

    ready

--- api/routes/answers.js
+++ api/routes/answers.js
@@ -8,56 +8,55 @@ const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "12345678",
-  database: "deneme"
+  database: "deneme",
 });
 
 router.get("/", (req, res, next) => {
-    const answer = {
-      exam_no: req.body.examNo,
-      answer_list: req.body.answerList
-    };
-    const queryString = "SELECT * FROM answers";
-    connection.query(queryString, null, (err, rows, fields) => {
-      const answers = rows.map((item) => {
-        return {
-            examNo: item.exam_no,
-            answerList: item.answer_list
-        };
-      });
-      res.json(answers);
+  const answer = {
+    exam_no: req.body.examNo,
+    answer_list: req.body.answerList,
+  };
+  const queryString = "SELECT * FROM answer";
+  connection.query(queryString, null, (err, rows, fields) => {
+    const answers = rows.map((item) => {
+      return {
+        examNo: item.exam_no,
+        answerList: item.answer_list,
+      };
     });
+    res.json(answers);
   });
+});
 
 router.get("/:examNo", (req, res, next) => {
-    const id = req.params.examNo;
-    const queryString = "SELECT * FROM answers WHERE exam_no=?";
-    connection.query(queryString, [id], (err, rows, fields) => {
+  const id = req.params.examNo;
+  const queryString = "SELECT * FROM answer WHERE exam_no=?";
+  connection.query(queryString, [id], (err, rows, fields) => {
     const answers = {
-        examNo: rows[0].exam_no,
-        answerList: rows[0].answer_list
-      };
-      res.status(200).json(answers);
-    });
-  });
-
-  router.post("/", (req, res, next) => {
-    const answer = {
-        exam_no: req.body.examNo,
-        answer_list: req.body.answerList
+      examNo: rows[0].exam_no,
+      answerList: rows[0].answer_list,
     };
-    const queryString =
-      "INSERT INTO answers(exam_no,answer_list) VALUES(?,?)";
-    connection.query(
-      queryString,
-      [answer.exam_no, answer.answer_list],
-      (err, rows, fields) => {
-        if (err) {
-          res.json({ message: "ERROR" });
-        } else {
-          res.json({ message: "Successfully" });
-        }
-      }
-    );
+    res.status(200).json(answers);
   });
+});
+
+router.post("/", (req, res, next) => {
+  const answer = {
+    exam_no: req.body.examNo,
+    answer_list: req.body.answerList,
+  };
+  const queryString = "INSERT INTO answer(exam_no,answer_list) VALUES(?,?)";
+  connection.query(
+    queryString,
+    [answer.exam_no, answer.answer_list],
+    (err, rows, fields) => {
+      if (err) {
+        res.json({ message: "ERROR" });
+      } else {
+        res.json({ message: "Successfully" });
+      }
+    }
+  );
+});
 
 module.exports = router;
--- api/routes/exams.js
+++ api/routes/exams.js
@@ -8,7 +8,7 @@ const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "12345678",
-  database: "deneme"
+  database: "deneme",
 });
 
 //get exams by id
@@ -17,13 +17,13 @@ router.get("/:examNo", (req, res, next) => {
   const queryString =
     "SELECT students.name, students.surname, students.student_no, exams.correctAnswer, exams.wrongAnswer FROM exams INNER JOIN students ON exams.student_no = students.student_no WHERE exams.exam_no=?";
   connection.query(queryString, [examNo], (err, rows, fields) => {
-    const students = rows.map(item => {
+    const students = rows.map((item) => {
       return {
         name: item.name,
         surname: item.surname,
         studentNo: item.student_no,
         correctAnswer: item.correctAnswer,
-        wrongAnswer: item.wrongAnswer
+        wrongAnswer: item.wrongAnswer,
       };
     });
     res.json(students);
@@ -33,20 +33,33 @@ router.get("/:examNo", (req, res, next) => {
 router.get("/:examNo/:studentNo", (req, res, next) => {
   const examNo = req.params.examNo;
   const studentNo = req.params.studentNo;
+  const queryString2 = "SELECT * FROM answer WHERE exam_no =?";
+  let answerData = {};
+  connection.query(queryString2, [examNo], (err, rows, fields) => {
+    const answerList = {
+      examNo: rows[0].exam_no,
+      examAnswer: rows[0].answer_list,
+    };
+    answerData = answerList;
+  });
   const queryString =
-    "SELECT students.name, students.surname, students.student_no, exams.correctAnswer, exams.wrongAnswer FROM exams INNER JOIN students ON exams.student_no = students.student_no WHERE exams.exam_no=? and students.student_no =?";
+    "SELECT students.name, students.surname, students.student_no, exams.correctAnswer, exams.studentAnswer, exams.wrongAnswer, exams.examURL FROM exams INNER JOIN students ON exams.student_no = students.student_no WHERE exams.exam_no=? and students.student_no =?";
   connection.query(queryString, [examNo, studentNo], (err, rows, fields) => {
-    const students = rows.map(item => {
+    const students = rows.map((item) => {
       return {
         student: {
           name: item.name,
           surname: item.surname,
-          no: item.student_no
+          no: item.student_no,
         },
         exams: {
+          examNo: examNo,
           correctAnswer: item.correctAnswer,
-          wrongAnswer: item.wrongAnswer
-        }
+          wrongAnswer: item.wrongAnswer,
+          studentAnswer: item.studentAnswer,
+          examAnswers: answerData,
+          examURL: item.examURL,
+        },
       };
     });
     res.json(students);
@@ -60,8 +73,10 @@ router.post("/", (req, res, next) => {
     studentName: req.body.studentName,
     studentSurname: req.body.studentSurname,
     studentNo: req.body.studentNo,
+	studentAnswer: req.body.studentAnswer,
     correctAnswer: req.body.correctAnswer,
-    wrongAnswer: req.body.wrongAnswer
+    wrongAnswer: req.body.wrongAnswer,
+    examURL: req.body.examURL,
   };
   const queryStudent = "SELECT * FROM students WHERE student_no=?";
   connection.query(queryStudent, [exam.studentNo], (err, rows, fields) => {
@@ -91,11 +106,18 @@ studentAdd = (exam, res) => {
 
 examAdd = (exam, res) => {
   const queryString =
-    "INSERT INTO exams(exam_no,student_no,correctAnswer, wrongAnswer) VALUES(?,?,?,?)";
+    "INSERT INTO exams(exam_no,student_no,correctAnswer, wrongAnswer,studentAnswer,examURL) VALUES(?,?,?,?,?,?)";
 
   connection.query(
     queryString,
-    [exam.examNo, exam.studentNo, exam.correctAnswer, exam.wrongAnswer],
+    [
+      exam.examNo,
+      exam.studentNo,
+      exam.correctAnswer,
+      exam.wrongAnswer,
+	  exam.studentAnswer,
+      exam.examURL,
+    ],
     (err, rows, fields) => {
       if (err) {
         res.json({ message: "ERROR" });
